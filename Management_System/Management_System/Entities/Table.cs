36a77115682a5ab37178f5e65fd235256d87ce59
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-10-29 11:41:15 +0300

    refactor: change blank lines

--- Management_System/Management_System/Entities/Table.cs
+++ Management_System/Management_System/Entities/Table.cs
@@ -7,6 +7,7 @@
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public Guid BoardId { get; set; }
+
         public IEnumerable<Task> Tasks { get; set; } = [];
     }
 }
--- Management_System/Management_System/Entities/User.cs
+++ Management_System/Management_System/Entities/User.cs
@@ -8,7 +8,6 @@
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
 
-
         public IEnumerable<UserBoard> UserBoards { get; set; } = [];
         public IEnumerable<UserTask> UserTasks { get; set; } = [];
     }
