737a1f518a87dcd4711cc5c6727b5d0c1a0f99bd
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-10-31 16:39:01 +0300

    feat: add db context

---
+++ Management_System/Management_System/Context/ManagementSystemDBContext.cs
@@ -0,0 +1,35 @@
+﻿using ManagementSystem.Entities;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManagementSystem.Context
+{
+    public class ManagementSystemDBContext : DbContext
+    {
+        public DbSet<Board> Boards { get; set; }
+        public DbSet<Label> Labels { get; set; }
+        public DbSet<Subtask> Subtasks { get; set; }
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<TaskEntity> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserBoard> UserBoards { get; set; }
+        public DbSet<UserTask> UserTasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            DataGenerator.Init();
+
+            modelBuilder.Entity<Board>().HasData(DataGenerator.Boards);
+            modelBuilder.Entity<Table>().HasData(DataGenerator.Tables);
+            modelBuilder.Entity<TaskEntity>().HasData(DataGenerator.Tasks);  
+            modelBuilder.Entity<Label>().HasData(DataGenerator.Labels);
+            modelBuilder.Entity<User>().HasData(DataGenerator.Users);
+            modelBuilder.Entity<Subtask>().HasData(DataGenerator.Subtasks);
+            modelBuilder.Entity<UserBoard>().HasData(DataGenerator.UserBoards);
+            modelBuilder.Entity<UserTask>().HasData(DataGenerator.UserTasks);
+
+        }
+    }
+}
---
+++ Management_System/Management_System/DI/ServicesConfguration.cs
@@ -0,0 +1,13 @@
+﻿using ManagementSystem.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManagementSystem.DI
+{
+    public static class ServicesConfguration
+    {
+        public static void AddDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ManagementSystemDBContext>(options => options.UseNpgsql(configuration.GetConnectionString("DBConnection")));
+        }
+    }
+}
