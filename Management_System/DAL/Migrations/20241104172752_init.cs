0eb269d97e683f5e647a9c543838528cf5dc83ee
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-11-05 23:52:23 +0300

    feat: move migrations to dal

--- Management_System/DAL/Migrations/20241104172752_init.cs
+++ Management_System/DAL/Migrations/20241104172752_init.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace ManagementSystem.Migrations
+namespace DAL.Migrations
 {
     /// <inheritdoc />
     public partial class init : Migration
