6cb41047c3e2a2f201337cb5c21713041fcd601d
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-11-15 14:19:50 +0300

    feat: add serialization

--- Management_System/Management_System/Program.cs
+++ Management_System/Management_System/Program.cs
@@ -1,9 +1,11 @@
 using ManagementSystem.DI;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApiDependencies(builder.Configuration);
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
