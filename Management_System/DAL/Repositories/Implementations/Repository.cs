6dc76c407b0b2ca88f3cc6746ac3e7d5d5583fa0
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-11-15 14:16:23 +0300

    feat: add repository

---
+++ Management_System/DAL/Repositories/Implementations/Repository.cs
@@ -0,0 +1,36 @@
+﻿using DAL.Context;
+using DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories.Implementations;
+public class Repository<T>(ManagementSystemDBContext db) : IRepository<T> where T : class
+{
+    public IQueryable<T> GetAll(CancellationToken cancellationToken)
+    {
+        return db.Set<T>();
+    }
+
+    public IQueryable<T> GetAll(int page, int pageSize, CancellationToken cancellationToken)
+    {
+        return db.Set<T>()
+                 .Skip((page - 1) * pageSize)
+                 .Take(pageSize)
+                 .AsNoTracking();
+    }
+
+    public async Task Add(T entity, CancellationToken cancellationToken)
+    {
+        await db.AddAsync(entity, cancellationToken);
+        await db.SaveChangesAsync(cancellationToken);
+    }
+    public async Task Update(T entity, CancellationToken cancellationToken)
+    {
+        db.Update(entity);
+        await db.SaveChangesAsync(cancellationToken);
+    }
+    public async Task Delete(T entity, CancellationToken cancellationToken)
+    {
+        db.Remove(entity);
+        await db.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ Management_System/DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace DAL.Repositories.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    IQueryable<T> GetAll(CancellationToken cancellationToken);
+    IQueryable<T> GetAll(int page, int pageSize, CancellationToken cancellationToken);
+    Task Add(T entity, CancellationToken cancellationToken);
+    Task Update(T entity, CancellationToken cancellationToken);
+    Task Delete(T entity, CancellationToken cancellationToken);
+}
