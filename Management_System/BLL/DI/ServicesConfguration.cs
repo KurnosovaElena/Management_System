5563d75167968d90ef33847a2f27e87dae273686
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-11-19 01:12:48 +0300

    fix: code style

--- Management_System/BLL/DI/ServicesConfguration.cs
+++ Management_System/BLL/DI/ServicesConfguration.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BLL.DI;
+
 public static class ServicesConfguration
 {
     public static void AddBusinessLogicDependencies(this IServiceCollection services, IConfiguration configuration)
--- Management_System/DAL/Repositories/Interfaces/IBoardRepository.cs
+++ Management_System/DAL/Repositories/Interfaces/IBoardRepository.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.Repositories.Interfaces;
+
 public interface IBoardRepository : IRepository<BoardEntity>
 {
     Task<IEnumerable<BoardEntity>> GetBoards(CancellationToken cancellationToken);
