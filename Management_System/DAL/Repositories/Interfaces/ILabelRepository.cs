a9996582ce6a61ee7848a12ab765ff22cebb7ed7
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-11-20 12:11:31 +0300

    feat: add label repository

---
+++ Management_System/DAL/Repositories/Interfaces/ILabelRepository.cs
@@ -0,0 +1,8 @@
+﻿using DAL.Entities;
+
+namespace DAL.Repositories.Interfaces;
+
+public interface ILabelRepository : IRepository<LabelEntity>
+{
+    Task<LabelEntity> GetLabel(Guid id, CancellationToken cancellationToken);
+}
