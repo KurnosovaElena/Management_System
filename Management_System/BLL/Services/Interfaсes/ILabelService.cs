8e28b45abd10477598fed17eef89eed3f2e5b4ee
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-11-20 12:12:06 +0300

    feat: add service for label

---
+++ Management_System/BLL/Services/Interfaсes/ILabelService.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services.Interfaсes
+{
+    public interface ILabelService
+    {
+        Task<LabelEntity> GetById(Guid id, CancellationToken cancellationToken);
+        Task<IEnumerable<LabelEntity>> GetAll(CancellationToken cancellationToken);
+        Task<LabelEntity> Add(LabelEntity entity, CancellationToken cancellationToken);
+        Task<LabelEntity> Update(Guid id, LabelEntity entity, CancellationToken cancellationToken);
+        Task Delete(Guid id, CancellationToken cancellationToken);
+    }
+}
