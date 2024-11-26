b6016a2d16cbe33ea1338dce2851b4a870f1e0de
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-11-26 10:58:58 +0300

    feat: add task status service

---
+++ Management_System/BLL/Services/Interfaсes/ITaskStatusService.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Entities;
+
+namespace BLL.Services.Interfaсes;
+
+public interface ITaskStatusService
+{
+    Task<TaskStatusEntity> GetById(Guid id, CancellationToken cancellationToken);
+    Task<IEnumerable<TaskStatusEntity>> GetAll(CancellationToken cancellationToken);
+    Task<TaskStatusEntity> Add(TaskStatusEntity entity, CancellationToken cancellationToken);
+    Task<TaskStatusEntity> Update(Guid id, TaskStatusEntity entity, CancellationToken cancellationToken);
+    Task Delete(Guid id, CancellationToken cancellationToken);
+}
