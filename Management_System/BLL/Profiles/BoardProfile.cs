0e639c0eb7bf5d279954d7ab2515908f8e8fad3a
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-02 10:55:34 +0300

    feat: add board profile

---
+++ Management_System/BLL/Profiles/BoardProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.DTO.CreateDTO;
+using DAL.Entities;
+
+namespace BLL.Profiles;
+
+public class BoardProfile : Profile 
+{ 
+    public BoardProfile() 
+    { 
+        CreateMap<CreateBoardDTO, BoardEntity>(); 
+        CreateMap<BoardEntity, BoardDTO>(); 
+    } 
+}
