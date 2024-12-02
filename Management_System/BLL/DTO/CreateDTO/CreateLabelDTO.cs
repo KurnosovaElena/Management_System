4da747c757cf448f716cbe4b8b77eaa176317822
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-02 11:44:42 +0300

    feat: create label DTO

---
+++ Management_System/BLL/DTO/CreateDTO/CreateLabelDTO.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Entities;
+
+namespace BLL.DTO.CreateDTO;
+
+public class CreateLabelDTO
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public IEnumerable<TaskEntity> Tasks { get; set; } = [];
+}
+
---
+++ Management_System/BLL/DTO/LabelDTO.cs
@@ -0,0 +1,8 @@
+﻿using BLL.DTO.CreateDTO;
+
+namespace BLL.DTO;
+
+public class LabelDTO : CreateLabelDTO
+{
+    public Guid Id { get; set; }
+}
