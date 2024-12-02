c69f776afacaac95cfe2a37019d633dd55b751fa
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-02 14:15:26 +0300

    fix: change board DTO

--- Management_System/BLL/DTO/BoardDTO.cs
+++ Management_System/BLL/DTO/BoardDTO.cs
@@ -1,14 +1,8 @@
 using BLL.DTO.CreateDTO;
-using DAL.Entities;
 
-namespace BLL.DTO
+namespace BLL.DTO;
+
+public class BoardDTO : CreateBoardDTO
 {
-    public class BoardDTO : CreateBoardDTO
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public IEnumerable<TaskStatusEntity> TaskStatus { get; set; } = new List<TaskStatusEntity>();
-        public IEnumerable<UserBoardEntity> UserBoards { get; set; } = new List<UserBoardEntity>();
-    }
+    public Guid Id { get; set; }
 }
