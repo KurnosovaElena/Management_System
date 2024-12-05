ede0cd699a95afa800ae0b15bb09dc72a85cce4e
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-05 22:53:57 +0300

    feat: add task status DTO

---
+++ Management_System/BLL/DTO/CreateDTO/CreateTaskStatusDTO.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Entities;
+
+namespace BLL.DTO.CreateDTO;
+
+public class CreateTaskStatusDTO
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public Guid BoardId { get; set; }
+
+    public IEnumerable<TaskEntity> Tasks { get; set; } = [];
+}
