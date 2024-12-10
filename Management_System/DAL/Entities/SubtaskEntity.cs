b462bf580e40e0886a0cd94fbea071ddc08e2351
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-10 16:32:49 +0300

    refactor: update SubtaskEntity with IEntity

--- Management_System/DAL/Entities/SubtaskEntity.cs
+++ Management_System/DAL/Entities/SubtaskEntity.cs
@@ -1,6 +1,6 @@
 ﻿namespace DAL.Entities;
 
-public class SubtaskEntity
+public class SubtaskEntity : IEntity
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
