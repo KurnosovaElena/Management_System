e9b8815adde83e8de4e8724fd7286683455f1655
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-10 16:33:22 +0300

    feat: add task controller

---
+++ Management_System/Management_System/Controllers/TasksController.cs
@@ -0,0 +1,45 @@
+﻿using BLL.DTO.CreateDTO;
+using BLL.DTO;
+using BLL.Services.Interfaсes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/tasks")]
+public class TasksController(ITasksService service) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IEnumerable<TasksDTO>> GetAll(CancellationToken cancellationToken)
+    {
+        var tasks = await service.GetAll(cancellationToken);
+        return tasks;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<TasksDTO> GetById([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var task = await service.GetById(id, cancellationToken);
+        return task;
+    }
+
+    [HttpPost]
+    public async Task<TasksDTO> Add([FromBody] CreateTasksDTO entity, CancellationToken cancellationToken)
+    {
+        var task = await service.Add(entity, cancellationToken);
+        return task;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<TasksDTO> Update([FromRoute] Guid id, [FromBody] CreateTasksDTO entity, CancellationToken cancellationToken)
+    {
+        var task = await service.Update(id, entity, cancellationToken);
+        return task;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task Delete([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        await service.Delete(id, cancellationToken);
+    }
+}
