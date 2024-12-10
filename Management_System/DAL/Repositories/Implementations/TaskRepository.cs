c8bdc4c64adb006c153e04cb0ab6bdb8a1c32746
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-10 16:04:29 +0300

    feat: create task repository

---
+++ Management_System/DAL/Repositories/Implementations/TaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Context;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories.Implementations;
+
+public class TaskRepository(ManagementSystemDBContext db) : Repository<TaskEntity>(db), ITaskRepository
+{
+    public override async Task<TaskEntity?> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        return await db.Set<TaskEntity>()
+            .Include(b => b.Subtasks)
+            .Include(b => b.UserTasks)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(entity => entity.Id == id, cancellationToken);
+    }
+}
