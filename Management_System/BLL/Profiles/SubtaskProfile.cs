c574f6248e0822a9b7e6f5ec452cd9c65dc37038
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-12 13:54:19 +0300

    feat: add subtask profile

---
+++ Management_System/BLL/Profiles/SubtaskProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.DTO.CreateDTO;
+using DAL.Entities;
+
+namespace BLL.Profiles;
+
+public class SubtaskProfile : Profile
+{
+    public SubtaskProfile()
+    {
+        CreateMap<CreateSubtaskDTO, SubtaskEntity>();
+        CreateMap<SubtaskEntity, SubtaskDTO>();
+    }
+}
