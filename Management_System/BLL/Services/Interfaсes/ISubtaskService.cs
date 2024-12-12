d679a5f3e056cd34ed84a9cbf0ba8480172c3dc1
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-12 17:52:51 +0300

    feat: create subtask service

---
+++ Management_System/BLL/Services/Interfaсes/ISubtaskService.cs
@@ -0,0 +1,13 @@
+﻿using BLL.DTO;
+using BLL.DTO.CreateDTO;
+
+namespace BLL.Services.Interfaсes;
+
+public interface ISubtaskService
+{
+    Task<SubtaskDTO> GetById(Guid id, CancellationToken cancellationToken);
+    Task<IEnumerable<SubtaskDTO>> GetAll(CancellationToken cancellationToken);
+    Task<SubtaskDTO> Add(CreateSubtaskDTO entity, CancellationToken cancellationToken);
+    Task<SubtaskDTO> Update(Guid id, CreateSubtaskDTO entity, CancellationToken cancellationToken);
+    Task Delete(Guid id, CancellationToken cancellationToken);
+}
