56bb3fe1e93eac50ebcb3448447b60b131429dff
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-15 00:54:07 +0300

    fix: code style

--- Management_System/DAL/Repositories/Implementations/SubtaskRepository.cs
+++ Management_System/DAL/Repositories/Implementations/SubtaskRepository.cs
@@ -4,6 +4,7 @@ using DAL.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Repositories.Implementations;
+
 public class SubtaskRepository(ManagementSystemDBContext db) : Repository<SubtaskEntity>(db), ISubtaskRepository
 {
     public override async Task<SubtaskEntity?> GetById(Guid id, CancellationToken cancellationToken)
