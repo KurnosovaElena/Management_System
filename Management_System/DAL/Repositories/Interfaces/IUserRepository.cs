d858f45a8943c3ab36d83f431c55b3aaedfe3523
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-15 21:58:30 +0300

    feat: add user repository

---
+++ Management_System/DAL/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using DAL.Entities;
+
+namespace DAL.Repositories.Interfaces;
+
+public interface IUserRepository : IRepository<UserEntity>
+{
+    Task<UserEntity> GetById(Guid id, CancellationToken cancellationToken);
+}
