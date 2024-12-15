f73cffa17695509066284a1ae41ac1ba523b382f
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-15 22:00:34 +0300

    feat: add user controller

---
+++ Management_System/Management_System/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using BLL.DTO.CreateDTO;
+using BLL.DTO;
+using BLL.Services.Interfaсes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/users")]
+public class UserController(IUserService service) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IEnumerable<UserDTO>> GetAll(CancellationToken cancellationToken)
+    {
+        var users = await service.GetAll(cancellationToken);
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<UserDTO> GetById([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var user = await service.GetById(id, cancellationToken);
+        return user;
+    }
+
+    [HttpPost]
+    public async Task<UserDTO> Add([FromBody] CreateUserDTO entity, CancellationToken cancellationToken)
+    {
+        var user = await service.Add(entity, cancellationToken);
+        return user;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<UserDTO> Update([FromRoute] Guid id, [FromBody] CreateUserDTO entity, CancellationToken cancellationToken)
+    {
+        var user = await service.Update(id, entity, cancellationToken);
+        return user;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task Delete([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        await service.Delete(id, cancellationToken);
+    }
+}
