f5e1e3ab822cb6bda3a63e3b2c943a5b5431596a
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-16 11:59:46 +0300

    feat: add ExceptionMiddleware

---
+++ Management_System/Management_System/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using BLL.Exceptions;
+using API.Exceptions;
+using System.Net;
+
+namespace API.Middlewares;
+
+public class ExceptionMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+    private static async Task HandleExceptionAsync(HttpContext context, Exception ex)
+    {
+        var responseMessage = ex switch
+        {
+            NotFoundException => new ExceptionResponse(
+                (int)HttpStatusCode.NotFound,
+                ex.Message
+            ),
+
+            BadRequestException => new ExceptionResponse(
+                (int)HttpStatusCode.BadRequest,
+                ex.Message
+            ),
+
+            _ => new ExceptionResponse(
+                (int)HttpStatusCode.InternalServerError,
+                ex.Message
+            )
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = responseMessage.StatusCode;
+
+        await context.Response.WriteAsJsonAsync(responseMessage);
+    }
+}
