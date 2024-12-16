6ed31e87fff3ca8abc31edfd26b36d638ae4f8f1
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-16 12:01:52 +0300

    refactor: add exceptions to LabelService

--- Management_System/BLL/Services/Implementaiton/LabelService.cs
+++ Management_System/BLL/Services/Implementaiton/LabelService.cs
@@ -1,27 +1,34 @@
 ﻿using AutoMapper;
 using BLL.DTO;
 using BLL.DTO.CreateDTO;
+using BLL.Exceptions;
 using BLL.Services.Interfaсes;
 using DAL.Entities;
 using DAL.Repositories.Interfaces;
 
-namespace BLL.Services.Implementaiton;
+namespace BLL.Services.Implementation;
+
 public class LabelService(ILabelRepository repository, IMapper mapper) : ILabelService
 {
     public async Task<LabelDTO> Add(CreateLabelDTO entity, CancellationToken cancellationToken)
     {
-        var label = mapper.Map<LabelEntity>(entity);
-
-        await repository.Add(label, cancellationToken);
-
-        var labelDTO = mapper.Map<LabelDTO>(label);
-
-        return labelDTO;
+        try
+        {
+            var label = mapper.Map<LabelEntity>(entity);
+            await repository.Add(label, cancellationToken);
+            var labelDTO = mapper.Map<LabelDTO>(label);
+            return labelDTO;
+        }
+        catch (Exception ex)
+        {
+            throw new ApplicationException("An error occurred while adding the label.", ex);
+        }
     }
 
     public async Task<LabelDTO> GetById(Guid id, CancellationToken cancellationToken)
     {
-        var label = await repository.GetById(id, cancellationToken);
+        var label = await repository.GetById(id, cancellationToken)
+            ?? throw new NotFoundException("No label found");
 
         var labelDTO = mapper.Map<LabelDTO>(label);
 
@@ -30,7 +37,8 @@ public class LabelService(ILabelRepository repository, IMapper mapper) : ILabelS
 
     public async Task<IEnumerable<LabelDTO>> GetAll(CancellationToken cancellationToken)
     {
-        var labels = await repository.GetAll(cancellationToken);
+        var labels = await repository.GetAll(cancellationToken)
+            ?? throw new NotFoundException("No labels found");
 
         var labelsDTO = mapper.Map<IEnumerable<LabelDTO>>(labels);
 
@@ -39,9 +47,15 @@ public class LabelService(ILabelRepository repository, IMapper mapper) : ILabelS
 
     public async Task<LabelDTO> Update(Guid id, CreateLabelDTO entity, CancellationToken cancellationToken)
     {
-        var label = await repository.GetById(id, cancellationToken);
+        var label = await repository.GetById(id, cancellationToken)
+            ?? throw new NotFoundException("No label found");
 
-        mapper.Map(label, entity);
+        if (entity == null)
+        {
+            throw new BadRequestException("Invalid label data provided.");
+        }
+
+        mapper.Map(entity, label);
 
         await repository.Update(label, cancellationToken);
 
@@ -52,7 +66,8 @@ public class LabelService(ILabelRepository repository, IMapper mapper) : ILabelS
 
     public async Task Delete(Guid id, CancellationToken cancellationToken)
     {
-        var label = await repository.GetById(id, cancellationToken);
+        var label = await repository.GetById(id, cancellationToken)
+            ?? throw new NotFoundException("No label found");
 
         await repository.Delete(label, cancellationToken);
     }
