943bde316e5100248ab48d8888cc94014a9ee200
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-16 12:01:40 +0300

    refactor: add exceptions to BoardService

--- Management_System/BLL/Services/Implementaiton/BoardService.cs
+++ Management_System/BLL/Services/Implementaiton/BoardService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BLL.DTO;
 using BLL.DTO.CreateDTO;
+using BLL.Exceptions;
 using BLL.Services.Interfaces;
 using DAL.Entities;
 using DAL.Repositories.Interfaces;
@@ -11,18 +12,23 @@ public class BoardService(IBoardRepository repository, IMapper mapper) : IBoardS
 {
     public async Task<BoardDTO> Add(CreateBoardDTO entity, CancellationToken cancellationToken)
     {
-        var board = mapper.Map<BoardEntity>(entity);
-
-        await repository.Add(board, cancellationToken);
-
-        var boardDTO = mapper.Map<BoardDTO>(board);
-
-        return boardDTO;
+        try
+        {
+            var board = mapper.Map<BoardEntity>(entity);
+            await repository.Add(board, cancellationToken);
+            var boardDTO = mapper.Map<BoardDTO>(board);
+            return boardDTO;
+        }
+        catch (Exception ex)
+        {
+            throw new ApplicationException("An error occurred while adding the board.", ex);
+        }
     }
 
     public async Task<BoardDTO> GetById(Guid id, CancellationToken cancellationToken)
     {
-        var board = await repository.GetById(id, cancellationToken);
+        var board = await repository.GetById(id, cancellationToken)
+        ?? throw new NotFoundException("No board found");
 
         var boardDTO = mapper.Map<BoardDTO>(board);
 
@@ -31,7 +37,8 @@ public class BoardService(IBoardRepository repository, IMapper mapper) : IBoardS
 
     public async Task<IEnumerable<BoardDTO>> GetAll(CancellationToken cancellationToken)
     {
-        var boards = await repository.GetAll(cancellationToken);
+        var boards = await repository.GetAll(cancellationToken)
+            ?? throw new NotFoundException("No boards found");
 
         var boardsDTO = mapper.Map<IEnumerable<BoardDTO>>(boards);
 
@@ -40,21 +47,24 @@ public class BoardService(IBoardRepository repository, IMapper mapper) : IBoardS
 
     public async Task<BoardDTO> Update(Guid id, CreateBoardDTO entity, CancellationToken cancellationToken)
     {
-        var board = await repository.GetById(id, cancellationToken);
+        var board = await repository.GetById(id, cancellationToken)
+            ?? throw new NotFoundException("No board found");
+
+        if (entity == null) 
+        { 
+            throw new BadRequestException("Invalid board data provided."); 
+        }
 
         mapper.Map(entity, board);
-
         await repository.Update(board, cancellationToken);
-
         var boardDTO = mapper.Map<BoardDTO>(board);
-
         return boardDTO;
     }
 
-
     public async Task Delete(Guid id, CancellationToken cancellationToken)
     {
-        var board = await repository.GetById(id, cancellationToken);
+        var board = await repository.GetById(id, cancellationToken)
+                ?? throw new NotFoundException("No board found");
         await repository.Delete(board, cancellationToken);
     }
 }
