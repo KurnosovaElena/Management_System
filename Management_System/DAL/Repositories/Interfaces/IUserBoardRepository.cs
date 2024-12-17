9b041b27c7058d55e762d9cf752b45c1ea115a3e
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-12-17 22:06:18 +0300

    feat: create userBoard repository

---
+++ Management_System/DAL/Repositories/Interfaces/IUserBoardRepository.cs
@@ -0,0 +1,10 @@
+﻿using DAL.Entities;
+
+namespace DAL.Repositories.Interfaces;
+
+public interface IUserBoardRepository : IRepository<UserBoardEntity>
+{
+    Task<UserBoardEntity?> GetByUserIdAndBoardIdAsync(Guid userId, Guid boardId, CancellationToken cancellationToken);
+    Task<IEnumerable<UserBoardEntity>> GetBoardsByUserIdAsync(Guid userId, CancellationToken cancellationToken);
+    Task<IEnumerable<UserBoardEntity>> GetUsersByBoardIdAsync(Guid boardId, CancellationToken cancellationToken);
+}
