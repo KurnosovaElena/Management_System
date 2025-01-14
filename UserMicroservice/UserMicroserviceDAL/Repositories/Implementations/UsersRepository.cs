40f004f16463083f17cd31a4fe2978fdfe56694b
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2025-01-14 16:27:30 +0300

    feat: add repositories

---
+++ UserMicroservice/UserMicroserviceDAL/Repositories/Implementations/UsersRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserMicroserviceDAL.Context;
+using UserMicroserviceDAL.Entities;
+using UserMicroserviceDAL.Repositories.Interfaces;
+
+namespace UserMicroserviceDAL.Repositories.Implementations;
+
+public class UsersRepository(UserMicriserviceDBContext db) : Repository<UserEntity>(db), IUsersRepository
+{
+    public override async Task<UserEntity?> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        return await db.Set<UserEntity>()
+        //.Include(b => b.UserBoards)
+        //.Include(b => b.UserTasks)
+        .AsNoTracking()
+        .FirstOrDefaultAsync(entity => entity.Id == id, cancellationToken);
+    }
+}
