ee8ad592d782a1721ab79fe8f4102d0d03e89d69
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2025-01-14 16:40:53 +0300

    feat: add migration

---
+++ UserMicroservice/UserMicroserviceDAL/Migrations/20250114120925_init.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace UserMicroserviceDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { new Guid("43d03bd8-7d1c-46cd-a423-7ef570ca6c68"), "alice.smith@example.com", "Alice", "Smith" },
+                    { new Guid("5e2729ba-b48e-4b5f-ba59-d7f55bdac1bb"), "bob.johnson@example.com", "Bob", "Johnson" },
+                    { new Guid("ec5506ed-b06f-4c35-a248-724e0bd9cd45"), "ethan.jones@example.com", "Ethan", "Jones" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
