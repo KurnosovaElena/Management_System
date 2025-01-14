5214adfa61e029a0043995b566db9068b348da1a
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2025-01-14 16:21:58 +0300

    feat: add entities

---
+++ UserMicroservice/UserMicroserviceDAL/Entities/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace UserMicroserviceDAL.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+}
---
+++ UserMicroservice/UserMicroserviceDAL/Entities/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace UserMicroserviceDAL.Entities;
+
+public class UserEntity : BaseEntity
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+    //public IEnumerable<UserBoardEntity> UserBoards { get; set; } = new List<UserBoardEntity>();
+    //public IEnumerable<UserTaskEntity> UserTasks { get; set; } = new List<UserTaskEntity>();
+}
