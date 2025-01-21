d32181f90fd6841f0a49093be1af083bce7bb794
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2025-01-21 14:27:25 +0300

    refactor: update user entity

--- UserMicroservice/UserMicroserviceDAL/Entities/UserEntity.cs
+++ UserMicroservice/UserMicroserviceDAL/Entities/UserEntity.cs
@@ -5,7 +5,4 @@ public class UserEntity : BaseEntity
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-
-    //public IEnumerable<UserBoardEntity> UserBoards { get; set; } = new List<UserBoardEntity>();
-    //public IEnumerable<UserTaskEntity> UserTasks { get; set; } = new List<UserTaskEntity>();
 }
