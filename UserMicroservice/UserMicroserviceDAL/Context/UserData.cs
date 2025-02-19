4b3de5c7b089b87478baa9ad175c5c8959cdd439
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2025-02-19 16:53:24 +0300

    refactor: remove users data to static class

---
+++ UserMicroservice/UserMicroserviceDAL/Context/UserData.cs
@@ -0,0 +1,38 @@
+﻿using UserMicroserviceDAL.Entities;
+
+namespace UserMicroserviceDAL.Context;
+
+public static class UserData
+{
+    public static List<UserEntity> Users { get; } = new List<UserEntity>
+    {
+        new UserEntity
+        {
+            Id = Guid.Parse("6f9a39fb-6876-4dbf-9657-f1f29b519f4d"),
+            FirstName = "Alice",
+            LastName = "Smith",
+            Email = "alice.smith@example.com"
+        },
+        new UserEntity
+        {
+            Id = Guid.Parse("e3a1976b-83e4-4f65-9b8f-67a5c317f8a7"),
+            FirstName = "Bob",
+            LastName = "Johnson",
+            Email = "bob.johnson@example.com"
+        },
+        new UserEntity
+        {
+            Id = Guid.Parse("0f5ea429-2e73-4a0b-93c3-f3b8b0eafb31"),
+            FirstName = "Ethan",
+            LastName = "Jones",
+            Email = "ethan.jones@example.com"
+        },
+        new UserEntity
+        {
+            Id = Guid.Parse("9f5ea420-2e73-4a0b-93c3-f3b8b0eafb35"),
+            FirstName = "Ethany",
+            LastName = "Jones",
+            Email = "ethany.jones@example.com"
+        }
+    };
+}
--- UserMicroservice/UserMicroserviceDAL/Context/UserMicriserviceDBContext.cs
+++ UserMicroservice/UserMicroserviceDAL/Context/UserMicriserviceDBContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 using UserMicroserviceDAL.Entities;
 
 namespace UserMicroserviceDAL.Context;
@@ -9,7 +8,7 @@ public class UserMicriserviceDBContext : DbContext
     public UserMicriserviceDBContext(DbContextOptions options) : base(options)
     {
         Database.Migrate();
-    } 
+    }
 
     public DbSet<UserEntity> Users { get; set; }
 
@@ -17,34 +16,6 @@ public class UserMicriserviceDBContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
-        var users = new List<UserEntity>
-        {
-            new() {
-              Id = Guid.Parse("6f9a39fb-6876-4dbf-9657-f1f29b519f4d"),
-              FirstName = "Alic",
-              LastName = "Smith",
-              Email = "alice.smith@example.com"
-          },
-          new() {
-              Id = Guid.Parse("e3a1976b-83e4-4f65-9b8f-67a5c317f8a7"),
-              FirstName = "Bob",
-              LastName = "Johnson",
-              Email = "bob.johnson@example.com"
-          },
-          new() {
-              Id = Guid.Parse("0f5ea429-2e73-4a0b-93c3-f3b8b0eafb31"),
-              FirstName = "Ethan",
-              LastName = "Jones",
-              Email = "ethan.jones@example.com"
-          },
-          new() {
-              Id = Guid.Parse("9f5ea420-2e73-4a0b-93c3-f3b8b0eafb35"),
-              FirstName = "Ethany",
-              LastName = "Jones",
-              Email = "ethany.jones@example.com"
-          }
-        };
-
-        modelBuilder.Entity<UserEntity>().HasData(users);
+        modelBuilder.Entity<UserEntity>().HasData(UserData.Users);
     }
 }
